74e48831bcdb545dc4b2a38f72aa01b7f35bf4f2
Ujin <jenia1702@gmail.com>
2020-06-21 19:57:56 +0300

    2.0

--- react/confusion/src/components/MenuComponent.js
+++ react/confusion/src/components/MenuComponent.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
   function RenderMenuItem({dish, onClick}){
     return(
       <div className="col-12 col-md-5 m-1">
-        <Card onClick={() => onClick(dish.id)}>
-            <CardImg width="100%" src={dish.image} alt={dish.name}></CardImg>
-            <CardImgOverlay>
-                <CardTitle>{dish.name}</CardTitle>
-            </CardImgOverlay>
+        <Card>
+            <Link to={`/menu/${dish.id}`} >
+              <CardImg width="100%" src={dish.image} alt={dish.name}></CardImg>
+              <CardImgOverlay>
+                  <CardTitle>{dish.name}</CardTitle>
+              </CardImgOverlay>
+            </Link>
         </Card>
       </div>  
     );
@@ -18,14 +21,24 @@ import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
     const menu = props.dishes.map((dish) =>{
         return(
-            <RenderMenuItem key={dish.id} dish={dish} onClick={props.onClick} />
+            <RenderMenuItem key={dish.id} dish={dish} />
         )
     });
     
     return (
         <div className="container">
             <div className="row">
-                {menu} 
+              <Breadcrumb>
+                <BreadcrumbItem><Link to='/home'>Home</Link></BreadcrumbItem>
+                <BreadcrumbItem active>Menu</BreadcrumbItem>
+              </Breadcrumb>
+              <div className="col-12">
+                <h3>Menu</h3>
+                <hr/>
+              </div>
+            </div>
+            <div className="row">
+              {menu} 
             </div>
         </div>
     );
