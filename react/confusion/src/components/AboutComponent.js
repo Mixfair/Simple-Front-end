ca0d8895c14b0f2fa2f5a410ebdf5741c97326f1
Ujin <jenia1702@gmail.com>
2020-08-27 09:10:54 +0300

    v2.2

--- react/confusion/src/components/AboutComponent.js
+++ react/confusion/src/components/AboutComponent.js
@@ -1,7 +1,28 @@
 import React from 'react';
-import RenderLeader from './RenderLeaderComponent';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
+
+
+function RenderLeader(props) {
+    
+    if (props.lead == null) return <div></div>
+
+    return(
+        <Media key={props.lead.id}>
+            <Media left href="#">
+                <Media className="mr-3" object src={props.lead.image}/>
+            </Media>
+            <Media body>
+                <Media heading>
+                    {props.lead.name}
+                </Media>
+                <p>{props.lead.designation}</p>
+                <p>{props.lead.description}</p>
+            </Media>    
+        </Media>
+    );
+
+}
 
 function About(props) {
 
